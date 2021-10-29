e7318b69b8a84a0578e574dcf6b8b51acfe1f3d0
James Serra <Jamesserra4@gmail.com>
2021-10-29 17:10:52 -0400

    one round play

--- script.js
+++ script.js
@@ -24,5 +24,38 @@ function computerPlay() {
         };
   };
 
+function playRound(playerSelection, computerSelection) {
+    if(playerSelection === computerSelection) {
+        return 'It\'s a tie!';
+      } else if (playerSelection === 'rock') {
+        if (computerSelection === 'paper') {
+          computerScore++;
+          return 'Computer wins!';
+        } else {
+          playerScore++;
+          return 'You win!';
+        }
+      } else if (playerSelection === 'paper'){
+        if (computerSelection === 'scissors') {
+          computerScore++;
+          return 'Computer wins!';
+        } else {
+          playerScore++;
+          return 'You win!';
+        }
+      } else if (playerSelection === 'scissors') {
+        if (computerSelection === 'rock') {
+          computerScore++;
+          return 'Computer wins!';
+        } else {
+          playerScore++;
+          return 'You win!';
+        }
+      }
+};
+
 const playerSelection = getPlayerMove();
 const computerSelection = computerPlay();
+console.log(playRound(playerSelection, computerSelection));
+console.log("Your Score = " + playerScore);
+console.log("Computer Score = " + computerScore);
